1774ad2a41bc0b577324aa1539c8da88733fa020
Hai Pham <hai.pham@s3corp.com.vn>
2022-06-29 22:32:42 +0700

    Change QA url from heroku

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 import { YOUTUBE_KEY_API } from '../utils/const'
 
+const QA_URL = 'https://remi-backend1.herokuapp.com/'
+const LOCAL_URL = 'http://localhost:8000/'
+
 // User
-export const checkUser = (value) => axios.post('http://localhost:5035/userData', { value })
-export const createUser = (value) => axios.post('http://localhost:5035/userData/register', { value })
+export const checkUser = (value) => axios.post(`${QA_URL}userData`, { value })
+export const createUser = (value) => axios.post(`${QA_URL}userData/register`, { value })
 
 // Movies
 export const getMovieDetails = (id) => axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${YOUTUBE_KEY_API}`)
-export const getMovies = () => axios.get('http://localhost:5035/movie')
-export const shareMovies = (value) => axios.post('http://localhost:5035/movie/shared', { value })
+export const getMovies = () => axios.get(`${QA_URL}movie`)
+export const shareMovies = (value) => axios.post(`${QA_URL}movie/shared`, { value })
